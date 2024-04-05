82eae392e7a8a786ae1da64f8e2756893da79d2f
goebers <robert.laitila@gmail.com>
2024-04-05 23:38:01 +0300

    fix SEO & google fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,30 +1,32 @@
 export default defineNuxtConfig({
   ssr: false,
   target: 'static',
-  head: {
-    title: 'Robert Laitila - Fullstack Web Developer',
-    htmlAttrs: {
-      lang: 'en',
+  app: {
+    head: {
+      title: 'Robert Laitila - Fullstack Web Developer',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Robert Laitila portfolio',
+        },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css?family=Titillium+Web:wght@300&display=swap',
+          defer: true,
+          async: true,
+          preconnect: true,
+        },
+      ],
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'Robert Laitila portfolio',
-      },
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Titillium+Web:wght@300&display=swap',
-        defer: true,
-        async: true,
-        preconnect: true,
-      },
-    ],
   },
   css: ['@/assets/scss/main.scss'],
   vite: {
